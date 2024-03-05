8b166ddf731f31b255f80a15205d1e647cf21dc8
Mayya Matueva <mayamatueva@gmail.com>
2024-03-05 14:11:07 +0100

    fix pagination

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -10,8 +10,9 @@ interface Channel {
 
 const Channels: React.FC = () => {
   const [channels, setChannels] = useState<Channel[]>([]);
+  const [totalChannels, setTotalChannels] = useState<number>(0);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [pageSize, setPageSize] = useState<number>(10); // Number of items per page
+  const [pageSize, setPageSize] = useState<number>(10);
 
   useEffect(() => {
     const fetchChannels = async () => {
@@ -22,6 +23,7 @@ const Channels: React.FC = () => {
         if (response.ok) {
           const data = await response.json();
           setChannels(data.channels);
+          setTotalChannels(data.pagination.totalhits);
         } else {
           console.error("Failed to fetch channels");
         }
@@ -31,11 +33,11 @@ const Channels: React.FC = () => {
     };
 
     fetchChannels();
-  }, [currentPage, pageSize]); // Fetch channels when page or page size changes
+  }, [currentPage, pageSize]);
 
   const handlePageChange = (page: number, pageSize?: number) => {
     setCurrentPage(page);
-    setPageSize(pageSize || 10); // Set page size or default to 10
+    setPageSize(pageSize || 10);
   };
 
   return (
@@ -50,10 +52,10 @@ const Channels: React.FC = () => {
         ))}
       </ul>
       <Pagination
-        onChange={handlePageChange} // Pass the handlePageChange function
-        current={currentPage} // Set the current page
-        total={channels.length} // Set the total number of items (optional)
-        pageSize={pageSize} // Set the number of items per page
+        onChange={handlePageChange}
+        current={currentPage}
+        total={totalChannels}
+        pageSize={pageSize}
       />
     </div>
   );
--- src/components/Program.tsx
+++ src/components/Program.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from "antd";
 import React, { useState, useEffect } from "react";
 
 interface Program {
@@ -10,16 +11,20 @@ interface Program {
 
 const Programs: React.FC = () => {
   const [programs, setPrograms] = useState<Program[]>([]);
+  const [totalPrograms, setTotalPrograms] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
 
   useEffect(() => {
     const fetchPrograms = async () => {
       try {
         const response = await fetch(
-          "http://api.sr.se/api/v2/programs/?format=json"
+          `http://api.sr.se/api/v2/programs/?format=json&page=${currentPage}&size=${pageSize}`
         );
         if (response.ok) {
           const data = await response.json();
           setPrograms(data.programs);
+          setTotalPrograms(data.pagination.totalhits);
         } else {
           console.error("Failed to fetch programs");
         }
@@ -29,7 +34,12 @@ const Programs: React.FC = () => {
     };
 
     fetchPrograms();
-  }, []);
+  }, [currentPage, pageSize]);
+
+  const handlePageChange = (page: number, pageSize?: number) => {
+    setCurrentPage(page);
+    setPageSize(pageSize || 10);
+  };
 
   return (
     <div>
@@ -42,6 +52,12 @@ const Programs: React.FC = () => {
           </li>
         ))}
       </ul>
+      <Pagination
+        onChange={handlePageChange}
+        current={currentPage}
+        total={totalPrograms} 
+        pageSize={pageSize}
+      />
     </div>
   );
 };
